6df5407be4abf8f36fa69d5fcfb723cc841bd450
SickTiredEngineer <chno336@naver.com>
2023-09-21 06:41:33 +0900

    20230921 FirstCommit
    Connect  관리 Class를 만들어 UI동작도 함께 여기서 관리하게 됨

--- app/src/main/java/com/example/robot_teaching_pendant_app/MainActivity.kt
+++ app/src/main/java/com/example/robot_teaching_pendant_app/MainActivity.kt
@@ -3,13 +3,12 @@ package com.example.robot_teaching_pendant_app
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-//import android.content.SharedPreferences
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-//import android.view.View
-//import android.view.Window
+import android.view.View
 //import android.view.WindowManager
 //import android.widget.CompoundButton
 import android.widget.Toast
@@ -20,10 +19,13 @@ import com.example.robot_teaching_pendant_app.make.MakeActivity
 import com.example.robot_teaching_pendant_app.play.PlayActivity
 import com.example.robot_teaching_pendant_app.setup.SetupActivity
 import com.example.robot_teaching_pendant_app.databinding.ConnectActivityBinding
+import com.example.robot_teaching_pendant_app.system.ConnectHelper
+
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var connectHelper: ConnectHelper
 
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,30 +47,24 @@ class MainActivity : AppCompatActivity() {
         val conBinding = ConnectActivityBinding.inflate(layoutInflater)
         val connectViewer = binding.connectViewer
         connectViewer.addView(conBinding.root)
+        //기본적인 상태를 설정합니다. 연결 상태 창은 붉은 빛이 들어오며, Disconnect 버튼은 비활성화 시킨 상태입니다.
+        conBinding.stateConBox.setBackgroundResource(R.drawable.color_red_box)
+        conBinding.disconnectBt.isEnabled = false
 
-        /* conCount 변수는 현재 연결 진행 단계를 나타냅니다. 이 변수를 가준으로 UI 동작을 수행하게 됩니다.
-        연걸전 Disconnect 버튼은 FALSE 상태로 맞춘 후 , 연결이 되면 true로 변경합니다. */
-        var conCount: Int = 0
-        val connectBt = conBinding.connectBt
-
-        val disconnectBt = conBinding.disconnectBt
-        disconnectBt.isEnabled = false
-
-        //연결 상태를 문자로 표시해주는 Textview 바인딩
-        val stateConnect = conBinding.stateConnect
-        val statePower = conBinding.statePower
-        val stateDevice = conBinding.stateDevice
-        val stateSystem = conBinding.stateSystem
-        val stateRobOper = conBinding.stateRobotOperation
-
-        //연결 상태를 색으로 표현해주는 TextView 바인딩
-        val stateConBox = conBinding.stateConBox
-        stateConBox.setBackgroundResource(R.drawable.color_red_box)
-
-        val statePowerBox = conBinding.statePowerBox
-        val stateDeviceBox = conBinding.stateDeviceBox
-        val stateSystemBox = conBinding.stateSystemBox
-        val stateRobOperBox = conBinding.stateRobOperBox
+        //Connect Helper Class 를 통하여 연결 동작을 실행합니다. 자세한 코드는 해당 Class를 참고 하십시오.
+        connectHelper = ConnectHelper(
+            connectBt = conBinding.connectBt,
+            disconnectBt = conBinding.disconnectBt,
+            stateConnect = conBinding.stateConnect,
+            stateConBox = conBinding.stateConBox,
+            statePower = conBinding.statePower,
+            statePowerBox = conBinding.statePowerBox,
+            stateDevice = conBinding.stateDevice,
+            stateDeviceBox = conBinding.stateDeviceBox,
+            stateSystem = conBinding.stateSystem,
+            stateSystemBox = conBinding.stateSystemBox,
+            stateRobOperBox = conBinding.stateRobOperBox
+        )
 
 
         //Night Mode 구현 부분
@@ -93,126 +89,6 @@ class MainActivity : AppCompatActivity() {
                 editor.putBoolean("night",false)
                 editor.apply()
             }
-        }
-
-
-
-        //Connect 버튼 동작 설정, conCount 변수에 있는 카운트 값을 기반으로 동작이 분기합니다.
-        connectBt.setOnClickListener{
-            when(conCount){
-                // 첫 번째 동작 (Connect) -> Connect 를 진행하며 동작이 끝나면 Connect 버튼을 Control 로 변환합니다.
-                0 -> {
-                    //중복 클릭을 방지하기 위해 과정이 끝나기 전 까지 connect 버튼을 비활성화 합니다.
-                    connectBt.isEnabled = false
-
-                    //첫번째 클릭 시, Connect state 의 텍스트와 상태 상자 색깔을 변경합니다.
-                    stateConnect.setText(R.string.state_connecting)
-                    stateConBox.setBackgroundResource(R.drawable.color_yellow_box)
-
-                    //연결 과정 표현 (딜레이 이용)
-                    val mHandler = Handler(Looper.getMainLooper())
-                    mHandler.postDelayed({
-                        stateConnect.setText(R.string.state_connected)
-                        stateConBox.setBackgroundResource(R.drawable.color_green_box)
-
-                        connectBt.setText(R.string.str_control)
-                        disconnectBt.setBackgroundResource(R.drawable.color_red_box)
-
-
-                        //Connect,Disconnect 버튼을 다시 활성화 시킵니다.
-                        connectBt.isEnabled = true
-                        disconnectBt.isEnabled = true
-                    }, 3000L)
-                    conCount += 1
-                }
-
-                //두 번째 동작 (Control) -> 나머지 요소들에 대한 작업을 진행하고 동작이 끝나면 Connect 버튼을 Power Down 으로 변경합니다.
-                1 -> {
-                    //중복 클릭을 방지하기 위해 과정이 끝나기 전 까지 connect, Disconnect 버튼을 비활성화 합니다.
-                    connectBt.isEnabled = false
-                    disconnectBt.isEnabled = false
-
-                    val mHandler = Handler(Looper.getMainLooper())
-                    mHandler.postDelayed({
-                        statePower.setText(R.string.state_power_on)
-                        statePowerBox.setBackgroundResource(R.drawable.color_green_box)
-                    }, 1000L)
-
-                    mHandler.postDelayed({
-                        stateDevice.setText(R.string.state_device_on)
-                        stateDeviceBox.setBackgroundResource(R.drawable.color_green_box)
-                    }, 1500L)
-
-                    mHandler.postDelayed({
-                        stateSystem.setText(R.string.state_system_on)
-                        stateSystemBox.setBackgroundResource(R.drawable.color_green_box)
-                    }, 2000L)
-
-                    mHandler.postDelayed({
-                        stateRobOperBox.setBackgroundResource(R.drawable.color_green_box)
-                    }, 2500L)
-
-                    mHandler.postDelayed({
-                        connectBt.setText(R.string.str_power_down)
-                        connectBt.setBackgroundResource(R.drawable.color_yellow_box)
-
-                        //Connect,Disconnect 버튼을 다시 활성화 시킵니다.
-                        connectBt.isEnabled = true
-                        disconnectBt.isEnabled = true
-                    }, 3000L)
-                    conCount += 1
-                }
-
-                //세 번째 동작(Power Down) -> 초기 상태로 연결 UI를 되돌립니다.
-                2 -> {
-                    stateConnect.setText(R.string.state_disconnected)
-                    stateConBox.setBackgroundResource(R.drawable.color_red_box)
-
-                    statePower.setText(R.string.state_power_off)
-                    statePowerBox.setBackgroundResource(R.drawable.square_background_border)
-
-                    stateDevice.setText(R.string.state_device_off)
-                    stateDeviceBox.setBackgroundResource(R.drawable.square_background_border)
-
-                    stateSystem.setText(R.string.state_system_off)
-                    stateSystemBox.setBackgroundResource(R.drawable.square_background_border)
-
-                    stateRobOperBox.setBackgroundResource(R.drawable.square_background_border)
-
-                    connectBt.setText(R.string.str_connect)
-                    connectBt.setBackgroundResource(R.drawable.color_green_box)
-
-                    disconnectBt.setBackgroundResource(R.drawable.color_gray_box)
-                    disconnectBt.isEnabled = false
-                    conCount = 0
-                }
-            }
-        }
-
-        //연결 끊기 버튼은 conCount를 즉시 0으로 초기화 시키고 연결 UI를 초기 상태로 돌려놓습니다.
-        disconnectBt.setOnClickListener{
-
-            stateConnect.setText(R.string.state_disconnected)
-            stateConBox.setBackgroundResource(R.drawable.color_red_box)
-
-            statePower.setText(R.string.state_power_off)
-            statePowerBox.setBackgroundResource(R.drawable.square_background_border)
-
-            stateDevice.setText(R.string.state_device_off)
-            stateDeviceBox.setBackgroundResource(R.drawable.square_background_border)
-
-            stateSystem.setText(R.string.state_system_off)
-            stateSystemBox.setBackgroundResource(R.drawable.square_background_border)
-
-            stateRobOperBox.setBackgroundResource(R.drawable.square_background_border)
-
-            connectBt.setText(R.string.str_connect)
-            connectBt.setBackgroundResource(R.drawable.color_green_box)
-
-            disconnectBt.isEnabled = false
-            disconnectBt.setBackgroundResource(R.drawable.color_gray_box)
-
-            conCount = 0
         }
 
         //작업 화면 버튼을 클릭 시 동작으로, 해당 화면으로 이동합니다.
---
+++ app/src/main/java/com/example/robot_teaching_pendant_app/system/ConnectHelper.kt
@@ -0,0 +1,164 @@
+package com.example.robot_teaching_pendant_app.system
+
+import android.app.Activity
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.example.robot_teaching_pendant_app.R
+
+//생성자입니다. 나중에 필요 시, 추가적으로 제어상자의 정보 View 등을 추가해야 합니다.
+class ConnectHelper(
+    private val connectBt: Button,
+    private val disconnectBt: Button,
+    private val stateConnect: TextView,
+    private val stateConBox: View,
+    private val statePower: TextView,
+    private val statePowerBox: View,
+    private val stateDevice: TextView,
+    private val stateDeviceBox: View,
+    private val stateSystem: TextView,
+    private val stateSystemBox: View,
+    private val stateRobOperBox: View
+) {
+    private var conCount = 0
+
+
+    //메서드 미리 선언(?)
+    init {
+        setConnectBtOnClickListener()
+    }
+    init {
+        setDisconnectBtOnClickListener()
+    }
+
+    //connect 버튼의 클릭 리스너 동작입니다. Case는 아래에 구현해 놓았습니다.
+    private fun setConnectBtOnClickListener() {
+        connectBt.setOnClickListener {
+            when(conCount){
+                0 -> FirstClick()
+                1 -> SecondClick()
+                2 -> ThirdClick()
+            }
+        }
+    }
+
+    //Disconnect 버튼의 클릭 리스너 동작입니다. 별도의 Case는 없습니다.
+    private fun setDisconnectBtOnClickListener() {
+        disconnectBt.setOnClickListener {
+
+            stateConnect.setText(R.string.state_disconnected)
+            stateConBox.setBackgroundResource(R.drawable.color_red_box)
+
+            statePower.setText(R.string.state_power_off)
+            statePowerBox.setBackgroundResource(R.drawable.square_background_border)
+
+            stateDevice.setText(R.string.state_device_off)
+            stateDeviceBox.setBackgroundResource(R.drawable.square_background_border)
+
+            stateSystem.setText(R.string.state_system_off)
+            stateSystemBox.setBackgroundResource(R.drawable.square_background_border)
+
+            stateRobOperBox.setBackgroundResource(R.drawable.square_background_border)
+
+            connectBt.setText(R.string.str_connect)
+            connectBt.setBackgroundResource(R.drawable.color_green_box)
+
+            disconnectBt.isEnabled = false
+            disconnectBt.setBackgroundResource(R.drawable.color_gray_box)
+
+            conCount = 0
+        }
+    }
+
+
+    /* Connect 버튼을 클릭 했을 시,  conCount 변수의 값에 따라 단계별로 실행되는 코드입니다. 현재 딜레이 표현을 위한
+    Handler를 이용하여 메인 스레드에서 실행하지만, 이 후 연결과 함께 UI 동작을 구현할 때 runOnUiThread를 사용해야 합니다.*/
+    private fun FirstClick() {
+        //중복 클릭을 방지하기 위해 과정이 끝나기 전 까지 connect 버튼을 비활성화 합니다.
+        connectBt.isEnabled = false
+
+        //첫번째 클릭 시, Connect state 의 텍스트와 상태 상자 색깔을 변경합니다.
+        stateConnect.setText(R.string.state_connecting)
+        stateConBox.setBackgroundResource(R.drawable.color_yellow_box)
+
+        //연결 과정 표현 (딜레이 이용)
+        val mHandler = android.os.Handler(Looper.getMainLooper())
+        mHandler.postDelayed({
+            stateConnect.setText(R.string.state_connected)
+            stateConBox.setBackgroundResource(R.drawable.color_green_box)
+
+            connectBt.setText(R.string.str_control)
+            disconnectBt.setBackgroundResource(R.drawable.color_red_box)
+
+            //Connect,Disconnect 버튼을 다시 활성화 시킵니다.
+            connectBt.isEnabled = true
+            disconnectBt.isEnabled = true
+        }, 3000L)
+        conCount += 1
+    }
+
+    private fun SecondClick() {
+        //중복 클릭을 방지하기 위해 과정이 끝나기 전 까지 connect, Disconnect 버튼을 비활성화 합니다.
+        connectBt.isEnabled = false
+        disconnectBt.isEnabled = false
+
+        val mHandler = Handler(Looper.getMainLooper())
+        mHandler.postDelayed({
+            statePower.setText(R.string.state_power_on)
+            statePowerBox.setBackgroundResource(R.drawable.color_green_box)
+        }, 1000L)
+
+        mHandler.postDelayed({
+            stateDevice.setText(R.string.state_device_on)
+            stateDeviceBox.setBackgroundResource(R.drawable.color_green_box)
+        }, 1500L)
+
+        mHandler.postDelayed({
+            stateSystem.setText(R.string.state_system_on)
+            stateSystemBox.setBackgroundResource(R.drawable.color_green_box)
+        }, 2000L)
+
+        mHandler.postDelayed({
+            stateRobOperBox.setBackgroundResource(R.drawable.color_green_box)
+        }, 2500L)
+
+        mHandler.postDelayed({
+            connectBt.setText(R.string.str_power_down)
+            connectBt.setBackgroundResource(R.drawable.color_yellow_box)
+
+            //Connect,Disconnect 버튼을 다시 활성화 시킵니다.
+            connectBt.isEnabled = true
+            disconnectBt.isEnabled = true
+        }, 3000L)
+        conCount += 1
+    }
+    private fun ThirdClick() {
+
+        stateConnect.setText(R.string.state_disconnected)
+        stateConBox.setBackgroundResource(R.drawable.color_red_box)
+
+        statePower.setText(R.string.state_power_off)
+        statePowerBox.setBackgroundResource(R.drawable.square_background_border)
+
+        stateDevice.setText(R.string.state_device_off)
+        stateDeviceBox.setBackgroundResource(R.drawable.square_background_border)
+
+        stateSystem.setText(R.string.state_system_off)
+        stateSystemBox.setBackgroundResource(R.drawable.square_background_border)
+
+        stateRobOperBox.setBackgroundResource(R.drawable.square_background_border)
+
+        connectBt.setText(R.string.str_connect)
+        connectBt.setBackgroundResource(R.drawable.color_green_box)
+
+        disconnectBt.setBackgroundResource(R.drawable.color_gray_box)
+        disconnectBt.isEnabled = false
+        conCount = 0
+
+    }
+}
+
