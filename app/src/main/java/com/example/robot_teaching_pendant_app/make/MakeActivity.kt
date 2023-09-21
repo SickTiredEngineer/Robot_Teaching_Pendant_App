56f6c05476490a260f0d04ff0f161414cc17b8fa
SickTiredEngineer <chno336@naver.com>
2023-09-22 07:09:21 +0900

    20230922
    작업환경(Make) 내부 요소들 Fragment화

--- app/src/main/java/com/example/robot_teaching_pendant_app/make/MakeActivity.kt
+++ app/src/main/java/com/example/robot_teaching_pendant_app/make/MakeActivity.kt
@@ -10,12 +10,7 @@ import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import com.example.robot_teaching_pendant_app.MainActivity
 import com.example.robot_teaching_pendant_app.R
-import com.example.robot_teaching_pendant_app.databinding.JGlobalActivityBinding
-import com.example.robot_teaching_pendant_app.databinding.JJointActivityBinding
-import com.example.robot_teaching_pendant_app.databinding.JLocalActivityBinding
-import com.example.robot_teaching_pendant_app.databinding.JUserActivityBinding
 import com.example.robot_teaching_pendant_app.databinding.MakeActivityBinding
-import com.example.robot_teaching_pendant_app.databinding.MakeDefaultActivityBinding
 import com.example.robot_teaching_pendant_app.databinding.MakeExtActivityBinding
 import com.example.robot_teaching_pendant_app.play.PlayActivity
 import com.example.robot_teaching_pendant_app.setup.SetupActivity
@@ -31,55 +26,18 @@ class MakeActivity : AppCompatActivity() {
         //좌측 상단에 위치한 화면 이동 메뉴 버튼
         val makeMenuBt = binding.makeMenuBt
 
-        //우측에 나타나는 전체적인 화면 Binding 입니다.
-        val defBinding = MakeDefaultActivityBinding.inflate(layoutInflater)
-
-
-        //Jog Mode -> Smooth , Tick 을 선택할 때 사용하는 버튼입니다.
-        val makeSmoothBt = defBinding.makeSmoothBt
-        val makeTickBt = defBinding.makeTickBt
-
-        //Tick 모드 활성화 시 나오는 EditText 입니다.
-        val tickJoint = defBinding.tickJoint
-        val tickOri = defBinding.tickOri
-        val tickDist = defBinding.tickDist
-
-        //Tick 모드 활성화 시 나오는 EditText 설명 TextView 입니다.
-        val jogModeTv1 = defBinding.jogModeTv1
-        val jogModeTv2 = defBinding.jogModeTv2
-        val jogModeTv3 = defBinding.jogModeTv3
-
-        //기본적으로 Tick 모드에서 사용하는 요소들은 가려지게 설정합니다.
-        tickDist.isInvisible = true
-        tickJoint.isInvisible = true
-        tickOri.isInvisible = true
-        jogModeTv1.isInvisible = true
-        jogModeTv2.isInvisible = true
-        jogModeTv3.isInvisible = true
-
-
-        //조그(Global, Local, User, Joint) 를 선택하는 버튼들을 변수에 초기화 시킵니다.
-        val jogGlobalBt = defBinding.jogGlobalBt
-        val jogLocalBt = defBinding.jogLocalBt
-        val jogUserBt = defBinding.jogUserBt
-        val jogJointBt = defBinding.jogJointBt
-
-
-        //조그 컨트롤러가 출력될 화면을 변수에 초기화 합니다.
-        val jogViewer = defBinding.jogControllerView
-
-        //우측 하단에 위치한 조그 컨트롤 화면을 바인딩 합니다. 해당 화면은 조그 모드에 따라서 바뀌게 됩니다.
-        //jog Global
-        val jgBinding = JGlobalActivityBinding.inflate(layoutInflater)
-        val jlBinding = JLocalActivityBinding.inflate(layoutInflater)
-        val jUbinding = JUserActivityBinding.inflate(layoutInflater)
-        val jJbinding = JJointActivityBinding.inflate(layoutInflater)
 
 
         /* TREE를 제외한 우측에 요소들을 표시해주는 View 와 기본적으로 보여지는 화면(Default) 설정이며, 확장 버튼 클릭 시 ext Activity로 교체합니다. */
         val sideViewer = binding.sideView
-        sideViewer.removeAllViews()
-        sideViewer.addView(defBinding.root)
+
+        if (savedInstanceState == null) { // 처음 액티비티가 생성되었을 때만 프래그먼트 추가
+            val fragment = MakeDefault() // 여기서 YourFragment는 원하는 프래그먼트 클래스명으로 바꾸세요.
+            supportFragmentManager.beginTransaction()
+                .replace(sideViewer.id, fragment)
+                .commit()
+        }
+
 
         val makePowerBt = binding.makePowerBt
         val treeExtBt = binding.treeExtBt
@@ -89,105 +47,16 @@ class MakeActivity : AppCompatActivity() {
 
 
         //Ext tree 토글 버튼 클릭 시 동작입니다.
-        treeExtBt.setOnCheckedChangeListener{_,isChecked->
-            if(isChecked){
-                sideViewer.removeAllViews()
-                sideViewer.addView(extBinding.root)
-
-            }
-            else{
-                sideViewer.removeAllViews()
-                sideViewer.addView(defBinding.root)
-            }
-        }
-
-
-        //Jog Mode(Global, Local, User, Joint)를 선택하기 위한 버튼 동작을 정의
-        jogGlobalBt.setOnClickListener{
-            //다중 클릭 방지를 위해 한 번 클릭된 본인은 다른 버튼을 클릭 하기 전 까지 disable 시킵니다.
-            jogGlobalBt.isEnabled = false
-
-            //자신을 제외한 다른 버튼을 활성화 시킵니다.
-            jogLocalBt.isEnabled = true
-            jogUserBt.isEnabled = true
-            jogJointBt.isEnabled = true
-
-            jogViewer.removeAllViews()
-            jogViewer.addView(jgBinding.root)
-        }
-
-        jogLocalBt.setOnClickListener{
-            //다중 클릭 방지를 위해 한 번 클릭된 본인은 다른 버튼을 클릭 하기 전 까지 disable 시킵니다.
-            jogLocalBt.isEnabled = false
-
-            //자신을 제외한 다른 버튼을 활성화 시킵니다.
-            jogGlobalBt.isEnabled =true
-            jogUserBt.isEnabled = true
-            jogJointBt.isEnabled = true
-
-
-            jogViewer.removeAllViews()
-            jogViewer.addView(jlBinding.root)
-        }
-
-        jogUserBt.setOnClickListener{
-            //다중 클릭 방지를 위해 한 번 클릭된 본인은 다른 버튼을 클릭 하기 전 까지 disable 시킵니다.
-            jogUserBt.isEnabled = false
-            //자신을 제외한 다른 버튼을 활성화 시킵니다.
-            jogGlobalBt.isEnabled = true
-            jogLocalBt.isEnabled = true
-            jogJointBt.isEnabled = true
-
-            jogViewer.removeAllViews()
-            jogViewer.addView(jUbinding.root)
-        }
-
-        jogJointBt.setOnClickListener{
-            //다중 클릭 방지를 위해 한 번 클릭된 본인은 다른 버튼을 클릭 하기 전 까지 disable 시킵니다.
-            jogJointBt.isEnabled = false
-            //자신을 제외한 다른 버튼을 활성화 시킵니다.
-            jogGlobalBt.isEnabled = true
-            jogLocalBt.isEnabled = true
-            jogUserBt.isEnabled = true
-
-            jogViewer.removeAllViews()
-            jogViewer.addView(jJbinding.root)
-        }
-
-
-        makeSmoothBt.setOnClickListener{
-            //중복 클릭 방지를 위해 본인 버튼을 비활성화 시킵니다.
-            makeSmoothBt.isEnabled = false
-            makeSmoothBt.setBackgroundResource(R.drawable.color_red_box)
-
-            makeTickBt.isEnabled = true
-            makeTickBt.setBackgroundResource(R.drawable.square_background_border)
-
-            //Tick에 관련된 요소들이 보이지 않게 가립니다.
-            tickDist.isInvisible = true
-            tickJoint.isInvisible = true
-            tickOri.isInvisible = true
-            jogModeTv1.isInvisible = true
-            jogModeTv2.isInvisible = true
-            jogModeTv3.isInvisible = true
-        }
-
-        makeTickBt.setOnClickListener{
-            //중복 클릭 방지를 위해 본인 버튼을 비활성화 시킵니다.
-            makeTickBt.isEnabled = false
-            makeTickBt.setBackgroundResource(R.drawable.color_red_box)
-
-            makeSmoothBt.isEnabled = true
-            makeSmoothBt.setBackgroundResource(R.drawable.square_background_border)
-
-            //Tick에 관련된 요소들을 활성화 시킵니다.
-            tickDist.isVisible = true
-            tickJoint.isVisible = true
-            tickOri.isVisible = true
-            jogModeTv1.isVisible = true
-            jogModeTv2.isVisible= true
-            jogModeTv3.isVisible = true
-        }
+        treeExtBt.setOnCheckedChangeListener { _, isChecked ->
+            val fragment = if(isChecked) {
+                MakeExt()
+                } else {
+                    MakeDefault()
+                    }
+                    supportFragmentManager.beginTransaction()
+                        .replace(sideViewer.id, fragment)
+                        .commit()
+                }
 
 
         //좌측 상단에 위치한 Menu 버튼 동작, Dialog 형식으로 해당 화면으로 이동할 수 있는 기능
---
+++ app/src/main/java/com/example/robot_teaching_pendant_app/make/MakeDefault.kt
@@ -0,0 +1,189 @@
+package com.example.robot_teaching_pendant_app.make
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import com.example.robot_teaching_pendant_app.R
+import com.example.robot_teaching_pendant_app.databinding.FragmentMakeDefaultBinding
+import com.example.robot_teaching_pendant_app.databinding.JGlobalActivityBinding
+import com.example.robot_teaching_pendant_app.databinding.JJointActivityBinding
+import com.example.robot_teaching_pendant_app.databinding.JLocalActivityBinding
+import com.example.robot_teaching_pendant_app.databinding.JUserActivityBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class MakeDefault : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val defBinding = FragmentMakeDefaultBinding.inflate(layoutInflater)
+        // Inflate the layout for this fragment
+
+
+        //Jog Mode -> Smooth , Tick 을 선택할 때 사용하는 버튼입니다.
+        val makeSmoothBt = defBinding.makeSmoothBt
+        val makeTickBt = defBinding.makeTickBt
+
+        //Tick 모드 활성화 시 나오는 EditText 입니다.
+        val tickJoint = defBinding.tickJoint
+        val tickOri = defBinding.tickOri
+        val tickDist = defBinding.tickDist
+
+        //Tick 모드 활성화 시 나오는 EditText 설명 TextView 입니다.
+        val jogModeTv1 = defBinding.jogModeTv1
+        val jogModeTv2 = defBinding.jogModeTv2
+        val jogModeTv3 = defBinding.jogModeTv3
+
+        //기본적으로 Tick 모드에서 사용하는 요소들은 가려지게 설정합니다.
+        tickDist.isInvisible = true
+        tickJoint.isInvisible = true
+        tickOri.isInvisible = true
+        jogModeTv1.isInvisible = true
+        jogModeTv2.isInvisible = true
+        jogModeTv3.isInvisible = true
+
+
+        //조그(Global, Local, User, Joint) 를 선택하는 버튼들을 변수에 초기화 시킵니다.
+        val jogGlobalBt = defBinding.jogGlobalBt
+        val jogLocalBt = defBinding.jogLocalBt
+        val jogUserBt = defBinding.jogUserBt
+        val jogJointBt = defBinding.jogJointBt
+
+        //Jog Controller를 보여 줄 View 입니다.
+        val jogViewer = defBinding.jogControllerView
+
+            //Jog Mode(Global, Local, User, Joint)를 선택하기 위한 버튼 동작을 정의
+        jogGlobalBt.setOnClickListener{
+            //다중 클릭 방지를 위해 한 번 클릭된 본인은 다른 버튼을 클릭 하기 전 까지 disable 시킵니다.
+            val jgBinding = JGlobalActivityBinding.inflate(layoutInflater)
+            jogGlobalBt.isEnabled = false
+
+            //자신을 제외한 다른 버튼을 활성화 시킵니다.
+            jogLocalBt.isEnabled = true
+            jogUserBt.isEnabled = true
+            jogJointBt.isEnabled = true
+
+            jogViewer.removeAllViews()
+            jogViewer.addView(jgBinding.root)
+        }
+
+        jogLocalBt.setOnClickListener{
+            //다중 클릭 방지를 위해 한 번 클릭된 본인은 다른 버튼을 클릭 하기 전 까지 disable 시킵니다.
+            val jlBinding = JLocalActivityBinding.inflate(layoutInflater)
+            jogLocalBt.isEnabled = false
+
+            //자신을 제외한 다른 버튼을 활성화 시킵니다.
+            jogGlobalBt.isEnabled =true
+            jogUserBt.isEnabled = true
+            jogJointBt.isEnabled = true
+
+
+            jogViewer.removeAllViews()
+            jogViewer.addView(jlBinding.root)
+        }
+
+        jogUserBt.setOnClickListener{
+            //다중 클릭 방지를 위해 한 번 클릭된 본인은 다른 버튼을 클릭 하기 전 까지 disable 시킵니다.
+            val juBinding = JUserActivityBinding.inflate(layoutInflater)
+            jogUserBt.isEnabled = false
+            //자신을 제외한 다른 버튼을 활성화 시킵니다.
+            jogGlobalBt.isEnabled = true
+            jogLocalBt.isEnabled = true
+            jogJointBt.isEnabled = true
+
+            jogViewer.removeAllViews()
+            jogViewer.addView(juBinding.root)
+        }
+
+        jogJointBt.setOnClickListener{
+            //다중 클릭 방지를 위해 한 번 클릭된 본인은 다른 버튼을 클릭 하기 전 까지 disable 시킵니다.
+            val jJBinding = JJointActivityBinding.inflate(layoutInflater)
+            jogJointBt.isEnabled = false
+            //자신을 제외한 다른 버튼을 활성화 시킵니다.
+            jogGlobalBt.isEnabled = true
+            jogLocalBt.isEnabled = true
+            jogUserBt.isEnabled = true
+
+            jogViewer.removeAllViews()
+            jogViewer.addView(jJBinding.root)
+        }
+
+
+        makeSmoothBt.setOnClickListener{
+            //중복 클릭 방지를 위해 본인 버튼을 비활성화 시킵니다.
+            makeSmoothBt.isEnabled = false
+            makeSmoothBt.setBackgroundResource(R.drawable.color_red_box)
+
+            makeTickBt.isEnabled = true
+            makeTickBt.setBackgroundResource(R.drawable.square_background_border)
+
+            //Tick에 관련된 요소들이 보이지 않게 가립니다.
+            tickDist.isInvisible = true
+            tickJoint.isInvisible = true
+            tickOri.isInvisible = true
+            jogModeTv1.isInvisible = true
+            jogModeTv2.isInvisible = true
+            jogModeTv3.isInvisible = true
+        }
+
+        makeTickBt.setOnClickListener{
+            //중복 클릭 방지를 위해 본인 버튼을 비활성화 시킵니다.
+            makeTickBt.isEnabled = false
+            makeTickBt.setBackgroundResource(R.drawable.color_red_box)
+
+            makeSmoothBt.isEnabled = true
+            makeSmoothBt.setBackgroundResource(R.drawable.square_background_border)
+
+            //Tick에 관련된 요소들을 활성화 시킵니다.
+            tickDist.isVisible = true
+            tickJoint.isVisible = true
+            tickOri.isVisible = true
+            jogModeTv1.isVisible = true
+            jogModeTv2.isVisible= true
+            jogModeTv3.isVisible = true
+        }
+
+
+        return defBinding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MakeDefault.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MakeDefault().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
